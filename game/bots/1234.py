9609e6fc2ce96d8b1be913e50478ef29e925af0c
mikhail <mikhail1demidov@gmail.com>
2023-03-28 18:37:18 +0300

    карта и флагa

--- game/bots/1234.py
+++ game/bots/1234.py
@@ -2,11 +2,4 @@ import random
 
 
 def make_choice(x, y, field):
-    me = field[x][y]['player']
-
-    if me['inventory']:
-        return f"use {me['inventory'][0]['type']}"
-
-
-    return(["go_up"])
-    #return random.choice(["fire_up", "fire_down", "fire_left", "fire_right", "go_up", "go_down", "go_left", "go_right"])
+    return random.choice(["fire_up", "fire_down", "fire_left", "fire_right", "go_up", "go_down", "go_left", "go_right"])
--- game/start.py
+++ game/start.py
@@ -23,15 +23,12 @@ while True:
         r.publish('edota_frame', json.dumps(frame))
         time.sleep(0.1)
 
-
-
-
-     def countdown(num_of_secs):
+    def countdown(num_of_secs):
         while num_of_secs:
-          m, s = divmod(num_of_secs, 60)
-        min_sec_format = '{:02d}:{:02d}'.format(m, s)
-        print(min_sec_format, end='/r')
-        time.sleep(1)
-        num_of_secs -= 1
+           m, s = divmod(num_of_secs, 60)
+           min_sec_format = '{:02d}:{:02d}'.format(m, s)
+           print(min_sec_format, end='/r')
+           time.sleep(1)
+           num_of_secs -= 1
 
-    print('Countdown finished.')
+        print('Countdown finished.')
